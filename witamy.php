449eac733130a3f306729a6887d08500db24d4e2
kamilkaz6 <58182663+kamilkaz6@users.noreply.github.com>
2020-06-21 11:53:09 +0200

    Update witamy.php

--- witamy.php
+++ witamy.php
@@ -15,46 +15,9 @@ session_start();
 	
 	
 ?>
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-
-	<meta charset="utf-8">
-	<title>Coffee lovers</title>
-	<meta name="description" content="Blog na temat ckawy. Omówienie wybranych zagadnień wziązanych z kawą!">
-	<meta name="keywords" content="kawa, mleko, blog, przemyślenia">
-	<meta name="author" content="Kamila Zawadzka">
-
-	<meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
-
-	<link rel="stylesheet" href="main.css">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
-
-		<script src="https://www.google.com/recaptcha/api.js" async defer></script>
-
-	<!--[if lt IE 9]>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
-	<![endif]-->
-
-</head>
-
-<body>
-
-	<header>
-
-		<h1 class="logo">Coffee lovers</h1>
-
-		<nav id="topnav">
-
-			<ul class="menu">
-				<li><a href="index.php">Strona główna</a></li>
-				<li><a href="logowanie.php">Logowanie</a></li>
-				<li><a href="rejestracja.php">Rejestracja</a></li>
-			</ul>
-
-		</nav>
-	</header>
-	
+<?php
+require_once("./sekcje/naglowek.php");
+?>
 	<main>
 		
 		<article>
